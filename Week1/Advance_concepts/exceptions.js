67cc4ab73ea6484bf48a4ba3d9034bcdc8ba4514
Nabin Acharya <nabinacharya740@gmail.com>
2024-02-19 21:56:43 +0545

    exceptions

---
+++ Week1/Advance_concepts/exceptions.js
@@ -0,0 +1,40 @@
+// syntax related
+
+/**
+ console.log("Hello
+ when we run this
+ 
+ Output
+ * console.log("Hello
+            ^^^^^^
+ SyntaxError: Invalid or unexpected token
+ */
+
+// Runtime error
+// let x=5
+// console.log(x.toUpperCase());
+
+/**
+ * Output
+ * TypeError: x.toUpperCase is not a function
+ * x is not a string. how can i uppercase number
+ */
+
+//Logical
+let num=5
+for(i=-3;i<5;i++){
+    console.log(num/i);
+}
+
+/**Logical error 
+ *Logical error is a dividing by a 0 
+ * Output
+ * -1.6666666666666667
+    -2.5
+    -5
+    Infinity
+    5
+    2.5
+    1.6666666666666667      
+    1.25
+ */
