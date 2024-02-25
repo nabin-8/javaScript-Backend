7e6d3b7119a3c1a76a0f2449040d94c4771cd5a1
Nabin Acharya <nabinacharya740@gmail.com>
2024-02-25 19:31:24 +0545

    module

--- Week2/MongodbProject/models/student.model.js
+++ Week2/MongodbProject/models/student.model.js
@@ -10,6 +10,12 @@ const studentScheme=new mongoose.Schema({
     name:String,
     age:Number
 })
+const teacherScheme=new mongoose.Schema({
+    name:String,
+    age:Number,
+    collage:String
+})
 
 //Go ahead and create corresponding collection in DB
-mongoose.model("Student", studentScheme)
+module.exports=mongoose.model("Student", studentScheme)
+module.exports=mongoose.model("Teacher", teacherScheme)
