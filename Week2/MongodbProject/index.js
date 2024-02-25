8cfb5f2cae7f2c468d0be44c250ec20fb56c68ff
Nabin Acharya <nabinacharya740@gmail.com>
2024-02-25 14:04:34 +0545

    mongoos

--- Week2/MongodbProject/index.js
+++ Week2/MongodbProject/index.js
@@ -4,4 +4,14 @@ const mongoose=require('mongoose')
  * Write the code to connect with MongoDB
  */
 
-mongoose.connect("mongodb://localhost/be_demodb")
+mongoose.connect("mongodb://127.0.0.1/be_demodb")
+// insted of localhost we can also use 127.0.0.1
+
+const db=mongoose.connection //Start the connection with MongoDB
+
+db.on("error", ()=>{
+    console.log("Error while conection to DB");
+})
+db.once("open",()=>{
+    console.log("Connected to Mongodb");
+})
